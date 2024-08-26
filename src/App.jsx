a599fe32cc247da497420d120c1e9b5178add35b
minhduc2307 <minhduc23072003@gmail.com>
2024-08-26 12:30:38 +0700

    Build UI for Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function App() {
-    return <p className="font-bold text-lg">Movie App</p>;
+    return (
+        <div>
+            <header className="flex py-5 bg-slate-950 text-white justify-between px-8 text-[1vw] font-medium">
+                <div className="flex items-center">
+                    <h1 className="uppercase text-red-500 font-bold text-[1.4vw]">
+                        <a href="/">Mọt phim</a>
+                    </h1>
+                    <nav className="mx-6">
+                        <ul className="flex gap-4">
+                            <li>
+                                <a href="#!">Phim</a>
+                            </li>
+                            <li>
+                                <a href="#!">TV</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <a href="#!">
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </a>
+                </div>
+                <div className="flex items-center gap-5">
+                    <a href="#!">Đăng nhập</a>
+                    <a href="#!" className="h-11 px-5 bg-[#ffb700] text-[#171c28] flex items-center justify-center rounded-2xl ml-5">Đăng ký</a>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
