942b5ccd654fddd31ce9b3ff2b895c2ecb23a102
minhduc2307 <minhduc23072003@gmail.com>
2024-08-31 13:08:45 +0700

    Responsive Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,jsx}"],
-    theme: {
-        extend: {
-            height: {
-                custom: "calc(100vh - 88px)",
-            },
-            gridTemplateColumns: {
-                footer: "2fr 1fr 1fr 1fr",
-            },
-        },
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      height: {
+        custom: "calc(100vh - 88px)",
+      },
+      gridTemplateColumns: {
+        footer: "2fr 1fr 1fr 1fr",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
